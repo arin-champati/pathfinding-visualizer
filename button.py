513fd2afac3657d097fe250bec590ce51121f4c3
Arin Champati <champati.arin@gmail.com>
2020-07-23 19:22:12 -0400

    UNDO function now works. After algorithm is finished, pressing the screen results in UNDO. Dropdown menus only allow one to be dropped down at once. Added a config file.

---
+++ button.py
@@ -0,0 +1,26 @@
+import pygame as pg
+from config import Colors, Fonts
+
+# get the parameters needed to write text (surface and rectangle)
+def text_objects(text, font, color):
+    text_surface = font.render(text, True, color)
+    return text_surface, text_surface.get_rect()
+
+# create a button with specified macros. When pressed, run a pressed action. Otherwise, return default action
+def create_button(window, default_action, pressed_action, font_size, text, active_color, inactive_color, x, y, w, h):
+    mouse = pg.mouse.get_pos()
+
+    # check if the mouse location is within the button bounding box
+    if x < mouse[0] < x + w and y < mouse[1] < y + h:
+        pg.draw.rect(window, active_color, (x, y, w, h))
+        if pg.mouse.get_pressed()[0]:
+            return pressed_action()
+    else:
+        pg.draw.rect(window, inactive_color, (x, y, w, h))
+
+    small_text = pg.font.Font(Fonts.BUTTON, int(font_size))
+    text_surf, text_rect = text_objects(text, small_text, Colors.WHITE)
+    text_rect.center = ((x + (w/2), y + (h / 2)))
+    window.blit(text_surf, text_rect)
+
+    return default_action()
---
+++ config.py
@@ -0,0 +1,26 @@
+# color codes
+class Colors:
+    RED = (255, 0, 0)
+    GREEN = (163, 197, 199)
+    LIGHT_BLUE = (180, 210, 255)
+    LIGHTER_BLUE = (102, 140, 191)
+    DARK_BLUE = (41, 65, 97)
+    DARKER_BLUE = (19, 35, 55)
+    YELLOW = (248, 236, 194)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    DARK_PURPLE = (71, 41, 97)
+    LIGHT_PURPLE = (218, 180, 250)
+    BURNT_ORANGE = (206, 140, 117)
+    SALMON = (250, 188, 180)
+    GREY = (128, 128, 128)
+    LIGHT_GREY = (220, 220, 220)
+    TURQOISE = (64, 224, 208)
+
+class Fonts:
+    BUTTON = 'freesansbold.ttf'
+    HOME = 'freesansbold.ttf'
+
+class Speeds:
+    HIGHWAY_SPEED = 75
+    LOCAL_SPEED = 35
